407336c6103ef309c0f26287855a69ea49d5ccb5
20171105118 <s20171105118@mac22.local>
2018-09-28 10:03:20 +0800

    优化了界面，修改一部分程序
    优化了界面，修改一部分程序

--- ViewController.swift
+++ ViewController.swift
@@ -1,63 +1,55 @@
-//
-//  ViewController.swift
-//  JSQ_GYF
-//
-//  Created by gyf on 2018/9/20.
-//  Copyright © 2018年 GYF. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var resultLabel: UILabel!
     
     var SZ3:String = ""
     var SZ2:String = ""
     var FH:String = ""
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func SZ1(_ sender: UIButton) {
         let value = sender.currentTitle
         if value == "+" || value == "-" || value == "*" || value == "/" {
-            FH = "value"
+            FH = value
             return
         }
         else if value == "=" {
-        var result = 0
+            var result = 0
             switch FH {
-                case "+":
-                    result = Int(SZ3)! + Int(SZ2)!
-                case "-":
-                    result = Int(SZ3)! - Int(SZ2)!
-                case "*":
-                    result = Int(SZ3)! * Int(SZ2)!
-                case "/":
-                    result = Int(SZ3)! / Int(SZ2)!
-                default:
-                    result = 0
+            case "+":
+                result = Int(SZ3)! + Int(SZ2)!
+            case "-":
+                result = Int(SZ3)! - Int(SZ2)!
+            case "*":
+                result = Int(SZ3)! * Int(SZ2)!
+            case "/":
+                result = Int(SZ3)! / Int(SZ2)!
+            default:
+                result = 0
             }
-         resultLabel.text = "\(result)"
-        
-        return
-    }
-    if FH == "" {
-        SZ3 = SZ3 + "value"
-        resultLabel.text = SZ3
-    }
-    else {
-        SZ2 = SZ2 + "value"
-        resultLabel.text = SZ3
+            resultLabel.text = "\(result)"
+            
+            return
+        }
+        if FH == "" {
+            SZ3 = SZ3 + Int(value)!
+            resultLabel.text = SZ3
+        }
+        else {
+            SZ2 = SZ2 + Int(value)!
+            resultLabel.text = SZ3
         }
     }
 }
